a8abe48012ab35540f04a564b2592f12b246f226
Shiv Singh <shiv@srapsware.com>
2024-08-05 17:30:39 +0530

    Update the API URL in the getStrapiURL function to remove the "/api/games/" path. This change ensures that the correct API URL is used for making requests.

--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,53 @@ import Head from "next/head";
 import Meta from "@components/Meta";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
+
+import GameList from "@components/GameList";
+
+import BloList from "@components/BloList";
+
 import React, { useRef, useState } from "react";
 
-const Home = () => {
+const Home = ({ games, blogs }) => {
   return (
     <>
       <Head>
         <title>Lasthash</title>
         <meta name="description" content="" />
       </Head>
-      <Header HeaderType="header-absolute" />
+      <Header />
 
-      
+      <h3>Game list coming from API</h3>
+
+      <GameList />
+
+      <h3>Blog List</h3>
+
+      <BloList />
 
       <Footer />
     </>
   );
 };
+
+export async function getStaticProps() {
+  // Run API calls in parallel
+  const [gamesRes, blogsRes] = await Promise.all([
+    fetchAPI("/games/popular", {
+      sort: ["id:desc"],
+    }),
+    fetchAPI("/blogs", {
+      sort: ["id:desc"],
+    }),
+  ]);
+
+  return {
+    props: {
+      games: gamesRes,
+      blogs: blogsRes,
+    },
+    revalidate: 1,
+  };
+}
 
 export default Home;
